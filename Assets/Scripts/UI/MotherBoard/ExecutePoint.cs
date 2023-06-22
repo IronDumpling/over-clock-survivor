aa16c2776fa6b2239fedb8e56a4b09c133fc9bc7
Chuyue Zhang <70104294+IronDumpling@users.noreply.github.com>
2023-06-22 11:32:38 -0400

    Broken Cell Call

--- Assets/Scripts/UI/MotherBoard/ExecutePoint.cs
+++ Assets/Scripts/UI/MotherBoard/ExecutePoint.cs
@@ -1,56 +1,82 @@
 using System;
+using System.Drawing;
 using System.Collections;
 using System.Collections.Generic;
-using System.Drawing;
 using Common;
-using UnityEditor.PackageManager;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using Color = UnityEngine.Color;
 
 public class ExecutePoint : MonoSingleton<ExecutePoint>
 {
-    public LayerMask mask;
-
-    private Vector2 previousNormal = Vector2.zero;
-    RaycastHit2D[] hitInfoList = new RaycastHit2D[4];
+    private readonly float raycastLength = 40f;
 
     private void Update()
     {
         GenerateRayCast();
-        CheckRayCast();
     }
 
     private void GenerateRayCast()
     {
-        hitInfoList[0] = Physics2D.Raycast(transform.position, -transform.up, 0.6f, mask.value);
-        hitInfoList[1] = Physics2D.Raycast(transform.position, -transform.right, 0.6f, mask.value);
-        hitInfoList[2] = Physics2D.Raycast(transform.position, transform.up, 0.6f, mask.value);
-        hitInfoList[3] = Physics2D.Raycast(transform.position, transform.right, 0.6f, mask.value);
+        RaycastFromPosition(transform.position + Vector3.up * raycastLength);
+        RaycastFromPosition(transform.position + Vector3.down * raycastLength);
+        RaycastFromPosition(transform.position + Vector3.left * raycastLength);
+        RaycastFromPosition(transform.position + Vector3.right * raycastLength);
     }
 
-    private void CheckRayCast()
+    void RaycastFromPosition(Vector3 position)
     {
-        foreach (var hitInfo in hitInfoList)
+        // Create a pointer event data and set its position
+        PointerEventData eventData = new(EventSystem.current)
         {
-            if (hitInfo.collider != null)
-            {
-                if (previousNormal != hitInfo.normal)
-                {
-                    CellManager.Instance.ResetCellListRun();
-                }
-                previousNormal = hitInfo.normal;
+            position = position
+        };
 
-                hitInfo.transform.GetComponent<CardCell>()?.Execute();
+        // Perform the raycast
+        var results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, results);
+
+        // Check if specific game objects are hit
+        GameObject prevObj = null;
+        foreach (var result in results)
+        {
+            if(prevObj != result.gameObject)
+            {
+                CellManager.Instance.ResetCellListRun();
             }
+
+            prevObj = result.gameObject;
+
+            result.gameObject.transform.GetComponent<CardCell>()?.Execute();
         }
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.magenta;
-        Gizmos.DrawLine(transform.position,transform.position +  -transform.up * 0.6f);
-        Gizmos.DrawLine(transform.position, transform.position + transform.up * 0.6f);
-        Gizmos.DrawLine(transform.position, transform.position + -transform.right * 0.6f);
-        Gizmos.DrawLine(transform.position, transform.position + transform.right * 0.6f);
+        Gizmos.DrawLine(transform.position,transform.position +  -transform.up * raycastLength);
+        Gizmos.DrawLine(transform.position, transform.position + transform.up * raycastLength);
+        Gizmos.DrawLine(transform.position, transform.position + -transform.right * raycastLength);
+        Gizmos.DrawLine(transform.position, transform.position + transform.right * raycastLength);
     }
+
+    //public LayerMask mask;
+    //private Vector2 previousNormal = Vector2.zero;
+    //private RaycastHit2D[] hitInfoList = new RaycastHit2D[4];
+    //private void CheckRayCast()
+    //{
+    //    foreach (var hitInfo in hitInfoList)
+    //    {
+    //        if (hitInfo.collider != null)
+    //        {
+    //            if (previousNormal != hitInfo.normal)
+    //            {
+    //                CellManager.Instance.ResetCellListRun();
+    //            }
+    //            previousNormal = hitInfo.normal;
+
+    //            hitInfo.transform.GetComponent<CardCell>()?.Execute();
+    //        }
+    //    }
+    //}
 }
